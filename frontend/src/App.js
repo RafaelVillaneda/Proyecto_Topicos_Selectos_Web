c2a4e68b9cec81193af4573aeefb5982503d2ddd
Lucy <71149886+RafaelVillaneda@users.noreply.github.com>
2023-04-22 16:43:47 -0600

    Se agrego el codigo al componente de registro
    Se agrego el componente de codigo funcionando mediante las rutas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,25 @@
 import './assets/estilos.css';
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import Landin from './components/landinPage';
 import Login from './components/login';
+import Registro from './components/registro';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Landin/>} />
-          <Route path='/login' element={<Login/>} />
-        </Routes>
-      </BrowserRouter>
+      <CSSTransition timeout={300}>
+          <BrowserRouter>
+            <Routes>
+              <Route path='/' element={<Landin />} />
+              <Route path='/login' element={<Login />} />
+              <Route path='/registro' element={<Registro />} />
+            </Routes>
+          </BrowserRouter>
+        </CSSTransition>
+
     </div>
+
   );
 }
 
--- frontend/src/components/registro.js
+++ frontend/src/components/registro.js
@@ -1,20 +1,15 @@
 import React from 'react'
-//import '../assets/login_css.css';
 import { Link } from 'react-router-dom';
-
-
-const login = () => {
-
-
-
+const registro = () => {
   return (
+    
     
     <div className="container">
       <div className="row justify-content-center mt-5">
         <div className="col-lg-6">
           <div className="card">
             <div className="card-header">
-              <h3 className="text-center">Inicio de Sesión</h3>
+              <h3 className="text-center">Regsitro de usuario</h3>
             </div>
             <div className="card-body">
               <form>
@@ -27,11 +22,15 @@ const login = () => {
                   <label for="exampleInputPassword1" className="form-label">Contraseña</label>
                   <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Ingrese su contraseña" />
                 </div>
+                <div className="mb-3">
+                  <label for="exampleInputPassword1" className="form-label">Confirma tu contraseña</label>
+                  <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Ingrese su contraseña" />
+                </div>
                 <button type="submit" className="btn btn-primary w-100">Iniciar Sesión</button>
               </form>
             </div>
             <div className="card-footer text-center">
-              ¿No tiene una cuenta? <Link to='/registro'>Regístrese aquí</Link>
+              Ya tienes una cuenta? <Link to='/login'>Inicia sección aqui!</Link>
               <br/> <br/>
               Oh bien ¿quieres volver a la pagina principal?<Link to='/'>Toca aqui!</Link>
             </div>
@@ -42,4 +41,4 @@ const login = () => {
   )
 }
 
-export default login
+export default registro
