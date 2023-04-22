c6e59d82559f05b98d4b23b255c8d5a450134908
Lucy <71149886+RafaelVillaneda@users.noreply.github.com>
2023-04-22 15:47:34 -0600

    Se emepezo a trabajar con el login
    Se cargo un asset de login en su respectivo componente "login.js"

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import '../assets/login_css.css';
+
 
 const login = () => {
+
+
+
   return (
-    <div>login</div>
+    <div className="login">
+      <h1>Login</h1>
+      <form method="post">
+        <input type="email" name="u" placeholder="Correo" required="required" />
+        <input type="password" name="p" placeholder="Password" required="required" />
+        <button type="submit" className="btn btn-primary btn-block btn-large">Let me in.</button>
+      </form>
+    </div>
   )
 }
 
