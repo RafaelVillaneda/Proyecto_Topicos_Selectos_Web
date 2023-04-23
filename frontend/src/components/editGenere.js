c2f0b28f124fc0b2f9a59952951365de6207116c
Lucy <71149886+RafaelVillaneda@users.noreply.github.com>
2023-04-23 15:47:17 -0600

    Se crearon los componentes inicio, editar Pelicula y editar genero

---
+++ frontend/src/components/editGenere.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const editGenere = () => {
+  return (
+    <div>editGenere</div>
+  )
+}
+
+export default editGenere
--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -4,9 +4,6 @@ import { Link } from 'react-router-dom';
 
 
 const login = () => {
-
-
-
   return (
     
     <div className="container">
